0131323a293c62670f92769fa0944df25ff8fb24
Климентий Пролецкий <klimentiy.proletskiy@gmail.com>
2023-07-04 23:02:25 +0300

    Deploy Foodgramand add workflow with PEP8 linting 15

--- backend/foodgram/management/commands/loading_csv_files.py
+++ backend/foodgram/management/commands/loading_csv_files.py
@@ -1,16 +1,18 @@
 import csv
-
+import os
 from backend_foodgram import settings
+
 from django.core.management.base import BaseCommand
 from foodgram.models import Ingredient
+
+CSV_PATH = os.path.join(settings.BASE_DIR, 'ingredients.csv')
 
 
 class Command(BaseCommand):
     help = ('Загружает данные в БД из csv-файлов.')
 
     def handle(self, *args, **options):
-        with open(settings.BASE_DIR + 'ingredients.csv',
-                  'r', newline='') as file_name:
+        with open(CSV_PATH, 'r', newline='') as file_name:
             print(file_name)
             reader = csv.DictReader(file_name)
             data = []
