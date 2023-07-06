b04d8b207855041428e729c9292072258a278e73
Климентий Пролецкий <klimentiy.proletskiy@gmail.com>
2023-07-06 15:04:08 +0300

    Deploy Foodgramand add workflow with PEP8 linting 21

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -237,7 +237,7 @@ class RecipeCreateSerializer(serializers.ModelSerializer):
                   'name', 'text',
                   'cooking_time', 'author')
         extra_kwargs = {
-            'ingredients': {'required': True, 'allow_blank': False},
+            'ingredients': {'required': True},
             'tags': {'required': True, 'allow_blank': False},
             'name': {'required': True, 'allow_blank': False},
             'text': {'required': True, 'allow_blank': False},
