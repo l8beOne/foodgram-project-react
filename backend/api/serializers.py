97c53471b5fee3743d207f5d8fe7a711b52bc9f5
Климентий Пролецкий <klimentiy.proletskiy@gmail.com>
2023-07-10 13:55:18 +0300

    Deploy Foodgramand add workflow with PEP8 linting 41

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -319,7 +319,10 @@ class RecipeCreateSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         ingredients = validated_data.pop('ingredients')
         tags = validated_data.pop('tags')
-        recipe = Recipe.objects.create(**validated_data)
+        recipe = Recipe.objects.create(
+            author=self.context['request'].user,
+            **validated_data
+        )
         recipe.tags.set(tags)
         self.ingredients_set(recipe, ingredients)
         return recipe
